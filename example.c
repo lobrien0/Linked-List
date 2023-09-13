e71b2304d9449f8f8e80285518be663dd48bd868
Luke <72243062+skymotic@users.noreply.github.com>
2023-09-12 18:43:01 -0600

    Update for doubly linked list

--- example.c
+++ example.c
@@ -1,32 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "linked_list.h"
 
 int main()
 {
-    struct Node *myList = list_init();
-    struct Node *listTwo = list_init();
-    // --- --- --- --- ---
-    list_add(myList, "Example Zero\0", 14);
-    list_add(myList, "Example One\0", 13);
-    list_add(myList, "Example Two\0", 13);
-    list_add(myList, "Example Three\0", 15);
-    list_add(myList, "Example Four\0", 14);
+    struct List *myList = list_init();
+    struct List *myList2 = list_init();
+    struct List *myList3 = list_init();
 
-    printf("----- List Printout -----\n");
-    list_print(myList);
-    list_remove(myList, 2);
+    printf("----- ----- -----\n");
+    // You can store strings quite easily
+    char myString[] = "Example Five";
 
-    printf("----- Node, Index 1 -----\n");
-    list_printNode(list_index(myList, 1));
+    list_add(myList, "Example Zero", 13);
+    list_add(myList, "Example One", 12);
+    list_add(myList, "Example Two", 12);
+    list_add(myList, "Example Three", 14);
+    list_add(myList, "Example Four", 13);
+    list_add(myList, myString, strlen(myString));
 
-    printf("----- List Printout -----\n");
-    list_print(myList);
+    // Dedicated print and get function for all primary data types.
+    list_printStr(myList);
+    char* strPtr = list_getStr(list_index(myList, 2));
+    printf("---\n%s\n", strPtr);
 
-    printf("----- -----\n");
-    printf("myList Count: %d\n", list_length(myList));
+    printf("----- ----- -----\n");
+    // Easily print out all details of a node for debugging.
+    list_printDetails(list_index(myList, 5));
 
-    printf("----- -----\n");
-    printf("listTwo Count: %d\n", list_length(listTwo));
+    printf("----- ----- -----\n");
+    // Easily store integers in the lists too.
+    int x;
+    for(x=0; x<11; x++)
+        list_add(myList2, &x, sizeof(int));
+    
+    list_printInt(myList2);
+
+    // Many functions to remove nodes
+    printf("--- --- ---\n");
+    list_removeFirst(myList2);
+    list_removeLast(myList2);
+    list_remove(myList2, 1);
+
+    list_printInt(myList2);
+
+    printf("----- ----- -----\n");
+    // Even easily store floats.
+    float y;
+    for(y=0; y<1; y=y+0.1)
+        list_add(myList3, &y, sizeof(float));
+    
+    list_printFloat(myList3);
+
+    printf("----- Error in Code -----\n");
+    // Functions to close and deallocate the entire list
+    list_close(myList3);
+    
+    // This line causes a segmentation fault because the list no longer exists
+    // We had deallocated all the nodes and the list itself.
+    list_printFloat(myList3);
+
+    printf("----- ----- -----\n");
+
+
 }
